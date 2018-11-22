76771cc4e7258a9b1b04eae538f59ce575582d11
cheng <792702352@qq.com>
2018-11-22 15:40:37 +0800

    :sparkles: 收集器用法详解

---
+++ src/main/java/com/cheng/jdk8/stream2/Student.java
@@ -0,0 +1,40 @@
+package com.cheng.jdk8.stream2;
+
+/**
+ * @author cheng
+ *         2018/11/21 15:22
+ */
+public class Student {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
