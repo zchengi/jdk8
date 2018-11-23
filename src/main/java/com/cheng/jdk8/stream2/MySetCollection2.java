2607a63243ab371c193e74b3e6be9e09265b6d97
cheng <792702352@qq.com>
2018-11-23 15:50:08 +0800

    :sparkles: 自定义收集器实现&深度剖析&并行流陷阱

---
+++ src/main/java/com/cheng/jdk8/stream2/MySetCollection2.java
@@ -0,0 +1,100 @@
+package com.cheng.jdk8.stream2;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * 自定义收集器深度剖析与并行流陷阱
+ *
+ * @author cheng
+ *         2018/11/23 14:53
+ */
+public class MySetCollection2<T> implements Collector<T, Set<T>, Map<T, T>> {
+
+    @Override
+    public Supplier<Set<T>> supplier() {
+        System.out.println("supplier invoked!");
+//        return HashSet<T>::new;
+
+        /*
+         *
+         *     A a1 = supplier.get();
+         *     accumulator.accept(a1, t1);
+         *     accumulator.accept(a1, t2);
+         *     R r1 = finisher.apply(a1);  // result without splitting
+         *
+         *     A a2 = supplier.get();
+         *     accumulator.accept(a2, t1);
+         *     A a3 = supplier.get();
+         *     accumulator.accept(a3, t2);
+         *     R r2 = finisher.apply(combiner.apply(a2, a3));  // result with splitting
+         *
+         */
+        return () -> {
+            System.out.println("----------");
+            return new HashSet<>();
+        };
+    }
+
+    @Override
+    public BiConsumer<Set<T>, T> accumulator() {
+        System.out.println("accumulator invoked!");
+        return (set, item) -> {
+            System.out.println("accumulator - " + Thread.currentThread().getName() + ", " + set);
+            set.add(item);
+        };
+    }
+
+    @Override
+    public BinaryOperator<Set<T>> combiner() {
+        System.out.println("combiner invoked!");
+
+        return (set1, set2) -> {
+            System.out.println("set1: " + set1);
+            System.out.println("set2: " + set2);
+            set1.addAll(set2);
+            return set1;
+        };
+    }
+
+    @Override
+    public Function<Set<T>, Map<T, T>> finisher() {
+        System.out.println("finisher invoked!");
+
+        return set -> {
+            Map<T, T> map = new HashMap<>();
+            set.forEach(item -> map.put(item, item));
+            return map;
+        };
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        System.out.println("characteristics invoked!");
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.UNORDERED
+                /*, Characteristics.IDENTITY_FINISH*/
+                /*, Characteristics.CONCURRENT*/));
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1; i++) {
+            List<String> list = Arrays.asList("hello", "world", "welcome", "a", "b", "c", "d", "e", "f", "g");
+
+            Set<String> set = new HashSet<>();
+            set.addAll(list);
+
+            System.out.println("set: " + set);
+
+//        Map<String, String> map = set.stream().collect(new MySetCollection2<>());
+            Map<String, String> map = set.parallelStream().collect(new MySetCollection2<>());
+            System.out.println("map: " + map);
+        }
+
+        System.out.println("线程核数：" + Runtime.getRuntime().availableProcessors());
+    }
+}
